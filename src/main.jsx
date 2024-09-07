448a70c8d49991af477463a6e0b4bad452b18933
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-07 21:30:48 +0545

    3D Portfolio

---
+++ src/main.jsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+//double click element then ctrl space for auto import 
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
