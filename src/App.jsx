544802ca0349dd8ca267c7df9a7e169a39281e92
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-07 22:58:44 +0545

    Added Nav bar and fixed navigations

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { Route, BrowserRouter as Router , Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 
+import {Home, About, Projects, Contact} from './pages'
+
 const App = () => {
     return (
      <main className='bg-slaate300/20'>
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/" element={'Home'} />
-                <Route path='/about' element={'About'}/>
-                <Route path='/projects' element={'Projects'}/>
-                <Route path='/contact' element={'Contact'}/>
+                <Route path="/" element={ <Home />} />
+                <Route path='/about' element={<About />}/>
+                <Route path='/projects' element={<Projects />}/>
+                <Route path='/contact' element={<Contact />}/>
             </Routes>
         </Router>
      </main>
