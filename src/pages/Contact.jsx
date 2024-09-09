36fe6ff49e07fb592b4deb9c83cf0c71c6e1d6eb
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-09-09 23:50:33 +0545

    Added Fox

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,12 @@
-import React, {  useRef, useState } from 'react';
-import emailjs from '@emailjs/browser'
+import React, { Suspense, useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { Canvas } from '@react-three/fiber';
+
+import Fox from '../models/Fox';
 
 
 const Contact = () => {
-  const formRef = useRef (null);
+  const formRef = useRef(null);
   const [form, setForm] = useState({ name: '', email: '', message: '' });
   const [isLoading, setIsLoading] = useState(false);
 
@@ -20,7 +23,7 @@ const Contact = () => {
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-      import.meta.env. VITE_APP_EMAILJS_TEMPLATE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
         from_name: form.name,
         to_name: "Ridan Shrestha",
@@ -43,15 +46,15 @@ const Contact = () => {
 
   };
 
-  const handleFocus = () => {};
-  const handleBlur = () => {};
+  const handleFocus = () => { };
+  const handleBlur = () => { };
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
-        <form 
+        <form
           className="w-full flex flex-col gap-7 mt-14"
           onSubmit={handleSubmit}
         >
@@ -107,6 +110,27 @@ const Contact = () => {
             {isLoading ? 'Sending...' : 'Send me a message'}
           </button>
         </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense>
+            <Fox
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   );
