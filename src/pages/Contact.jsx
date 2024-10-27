58cf41c64449147446be46b0a7d482ed4d17bb9f
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-27 19:55:17 +0545

    added popup message in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,13 +5,18 @@ import { Suspense, useRef, useState } from 'react';
 import Fox from '../models/Fox';
 
 import Loader from '../components/Loader';
+import useAlert from '../hooks/useAlert';
+import Alert from '../components/Alert';
 
 const Contact = () => {
   // const formRef = useRef(null);
   const formRef = useRef();
   const [form, setForm] = useState({ name: '', email: '', message: '' });
   const [isLoading, setIsLoading] = useState(false); 
+  // const [loading, setLoading] = useState(false); 
   const [currentAnimation, setCurrentAnimation] = useState('idle');
+
+  const { alert, showAlert, hideAlert} = useAlert();
 
   const handleChange = (e) => {
     setForm({
@@ -23,9 +28,10 @@ const Contact = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
-    setCurrentAnimation('hit');
+    setCurrentAnimation("hit");
 
-    emailjs.send(
+    emailjs
+      .send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
       import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
       {
@@ -36,25 +42,38 @@ const Contact = () => {
         message: form.message
       },
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
-    ).then(() => {
+    )
+    .then(
+      () => {
       setIsLoading(false);
+        showAlert({ show: true, text: 'Message sent successfully!', type:'success' });
+
 
       setTimeout(() => {
-        setCurrentAnimation('idle')
-        setForm({ name: '', email: '', message: '' });
+        hideAlert();
+        setCurrentAnimation("idle");
+        setForm({ 
+          name: '', 
+          email: '', 
+          message: '' 
+        });
       }, [3000]);
 
     }).catch((error) => {
       setIsLoading(false);
-      setCurrentAnimation('idle')
+      setCurrentAnimation("idle")
+      console.log(error);
+      showAlert({ show: true, text: 'I didnt receive your message!', type:'danger' });
     });
   };
 
-  const handleFocus = () => setCurrentAnimation('walk');
-  const handleBlur = () => setCurrentAnimation('idle');
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
 
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
+      {alert.show && <Alert {...alert} />}
+
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
@@ -127,6 +146,7 @@ const Contact = () => {
         >
           <directionalLight intensity={2.5} position={[0, 0, 1]} />
           <ambientLight intensity={0.5} />
+
           <Suspense fallback={<Loader />}>
             <Fox
               currentAnimation={currentAnimation}
