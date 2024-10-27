dd987c181fa369ba610ab74212ae363a4bdb8110
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-27 20:40:49 +0545

    added some info in about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { skills } from '../constants'
 
 const About = () => {
   return (
-    <div>
-      About
-    </div>
+   <section className="max-container">
+    <h1 className='head-text'>
+      Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Ridan</span>
+      </h1>
+
+      <div>
+        <p className='mt-5 flex flex-col gap-3 text-slate-500'>
+          Full stack developer with a passion for creating visually stunning 3D models and interactive experiences. I specialize in JavaScript, React, and WebGL. I'm always eager to learn and improve my skills. 
+        </p>
+      </div>
+
+     
+   </section>
   )
 }
 
