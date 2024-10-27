eb74c0e858c11854d54b6379bdb43f838b7e6d0c
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-27 23:27:20 +0545

    added classes to outer divs and styled them

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -19,8 +19,9 @@ const About = () => {
 
         <div className='mt-16 flex flex-wrap gap-12'>
           {skills.map((skill) => (
-           <div>
-            <div>
+           <div className='block-container w-20 h-20'>
+            <div className='btn-back rounded-xl' />
+            <div className='btn-front rounded-xl flex justify-center items-center'>
               <img 
               src={skill.imageUrl}
               alt={skill.name}
