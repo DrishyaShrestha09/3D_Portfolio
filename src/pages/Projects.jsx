aaeda4280accc51e4bcce5e11a21d5047586779f
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-28 15:09:53 +0545

    styed outer divs and added CTA

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 import { projects } from '../constants';
 import { arrow } from '../assets/icons';
+import CTA from '../components/CTA';
 
 const Projects = () => {
   return (
@@ -30,13 +31,13 @@ const Projects = () => {
             </div>
 
             <div className="mt-5 flex flex-col">
-              <h4>
+              <h4 className='text-2xl font-poppins font-semibold'>
                 {project.name}
               </h4>
-              <p>
+              <p className='mt-2 text-slate-500'>
                 {project.description}
               </p>
-              <div>
+              <div className='mt-5 flex items-center gap-2 font-poppins'>
                 <Link
                   to={project.link}
                   target='_blank'
@@ -55,6 +56,10 @@ const Projects = () => {
           </div>
         ))}
       </div>
+
+      <hr className='border-slate-200'/>
+
+      <CTA/>
     </section>
   )
 }
