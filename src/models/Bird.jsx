1f14272345887659f8be64f610d0ff331cb696fc
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-28 17:55:14 +0545

    fixed bird movement

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,56 +1,42 @@
-import { useRef, useEffect } from 'react';
-import { useAnimations, useGLTF } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
-import birdScene from '../assets/3d/bird.glb';
+import { useEffect, useRef } from "react";
+import { useFrame } from "@react-three/fiber";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
+import birdScene from "../assets/3d/bird.glb";
 
 const Bird = () => {
   const birdRef = useRef();
+
   const { scene, animations } = useGLTF(birdScene);
   const { actions } = useAnimations(animations, birdRef);
 
   useEffect(() => {
-    const action = actions['Take001'];
-    if (action) {
-      action.play();
-    }
+    actions["Take 001"].play();
   }, [actions]);
 
   useFrame(({ clock, camera }) => {
-    if (birdRef.current) {
-      // Update the Y position to simulate the flight moving in a sin wave
-      birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
+    birdRef.current.position.y = Math.sin(clock.elapsedTime) * 0.2 + 2;
 
-      // Check if the bird reached a certain endpoint relative to the camera
-      if (birdRef.current.position.x > camera.position.x + 10) {
-        // Change direction to backward and rotate the bird 180 degrees on the y-axis
-        birdRef.current.rotation.y = Math.PI;
-      } else if (birdRef.current.position.x < camera.position.x - 10) {
-        // Change direction to forward and reset the bird's rotation
-        birdRef.current.rotation.y = 0;
-      }
+    if (birdRef.current.position.x > camera.position.x + 10) {
+      birdRef.current.rotation.y = Math.PI;
+    } else if (birdRef.current.position.x < camera.position.x - 10) {
+      birdRef.current.rotation.y = 0;
+    }
 
-      // Update the X and Z positions based on the direction
-      if (birdRef.current.rotation.y === 0) {
-        // Moving forward
-        birdRef.current.position.x += 0.01;
-        birdRef.current.position.z -= 0.01;
-      } else {
-        // Moving backward
-        birdRef.current.position.x -= 0.01;
-        birdRef.current.position.z += 0.01;
-      }
+    if (birdRef.current.rotation.y === 0) {
+      birdRef.current.position.x += 0.01;
+      birdRef.current.position.z -= 0.01;
+    } else {
+      birdRef.current.position.x -= 0.01;
+      birdRef.current.position.z += 0.01;
     }
   });
 
   return (
-    <mesh 
-      position={[-5, 2, 1]} 
-      scale={[0.003, 0.003, 0.003]} 
-      ref={birdRef}
-    >
+    <mesh ref={birdRef} position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]}>
       <primitive object={scene} />
     </mesh>
   );
-};
+}
 
 export default Bird;
