43c7f79d454943330e901a68d80b81aff147b617
DrishyaShrestha09 <ridanstha09@gmail.com>
2024-10-28 17:40:55 +0545

    fixed bugs and isses

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,8 @@ const Home = () => {
 
   audioRef.current.volume = 0.4;
   audioRef.current.loop = true;
-  const [currentStage, setCurrentStage] = useState(1); // added useState
+
+  const [currentStage, setCurrentStage] = useState(1); 
   const [isRotating, setIsRotating] = useState(false);
   const [isPlayingMusic, setPlayingMusic] = useState(false);
 
@@ -35,23 +36,10 @@ const Home = () => {
     }
   }, [isPlayingMusic]);
 
-  const adjustIslandForScreenSize = () => {
-    let screenScale = null;
-    let screenPosition = [0, -6.5, -43];
-    let rotation = [0.1, 4.7, 0];
-
-    if (window.innerWidth < 768) {
-      screenScale = [0.9, 0.9, 0.9];
-    } else {
-      screenScale = [1, 1, 1];
-    }
-
-    return [screenScale, screenPosition, rotation];
-  };
-
-  const adjustPlaneForScreenSize = () => {
+ const adjustBiplaneForScreenSize = () => {
     let screenScale, screenPosition;
 
+    // If screen width is less than 768px, adjust the scale and position
     if (window.innerWidth < 768) {
       screenScale = [1.5, 1.5, 1.5];
       screenPosition = [0, -1.5, 0];
@@ -63,19 +51,33 @@ const Home = () => {
     return [screenScale, screenPosition];
   };
 
-  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
-  const [planeScale, planePosition] = adjustPlaneForScreenSize();
+  const adjustIslandForScreenSize = () => {
+    let screenScale, screenPosition;
 
-  // POPUP STAGE ON ROTATION
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
+    } else {
+      screenScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
+    }
+
+    return [screenScale, screenPosition];
+  };
+
+  const [biplaneScale, biplanePosition] = adjustBiplaneForScreenSize();
+  const [islandScale, islandPosition] = adjustIslandForScreenSize();
+
   return (
-    <section className="w-full h-screen relative">
-      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+    <section className='w-full h-screen relative'>
+      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
         {currentStage && <HomeInfo currentStage={currentStage} />}
       </div>
 
       <Canvas
-        className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"
-          }`}
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -106,19 +108,19 @@ const Home = () => {
           />
           <Plane
             isRotating={isRotating}
-            scale={planeScale}
-            position={planePosition}
-            rotation={[0, 20, 0]}
+            position={biplanePosition}
+            rotation={[0, 20.1, 0]}
+            scale={biplaneScale}
           />
         </Suspense>
       </Canvas>
 
-      <div className = "absolute bottom-2 left-2 " >
+      <div className='absolute bottom-2 left-2'>
         <img
           src={!isPlayingMusic ? soundoff : soundon}
-          alt="sound"
-          className="w-10 h-10 cursor-pointer object-contain"
-          onClick={() => setPlayingMusic(!isPlayingMusic)}
+          alt='jukebox'
+          onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+          className='w-10 h-10 cursor-pointer object-contain'
         />
       </div>
     </section>
